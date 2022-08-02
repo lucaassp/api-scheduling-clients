7e1ace1d2c66d8f3d0abeb4fc38d724aebb5fd32
lucaassp <lucaspereira.student@gmail.com>
2022-08-02 10:52:56 -0300

    Initial commit

---
+++ src/test/java/com/schedulingclients/SchedulingClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulingclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
